2901b5518d6c90e45e4afc98060b79ed7d896a4d
anom90 <111362537+anom90@users.noreply.github.com>
2026-05-18 12:50:12 +0700

    Add files via upload
    Upload Dataset dan Sintaks R Buku SEM Edisi 2 v1

---
+++ 1. Syntax_Install Paket.R
@@ -0,0 +1,13 @@
+install.packages(
+  c("openxlsx", 
+    "lavaan", 
+    "semPlot", 
+    "semTools", 
+    "semptools",
+    "remotes",
+    "BifactorIndicesCalculator"
+  )
+)
+
+# Update Paket lavaan
+remotes::install_github("yrosseel/lavaan")
